cbce78c5d3c2872ce7b069574fcd4da3d3364cce
EVR_NguyenTruongPhuc <phuc.nguyen@ever-rise.co.jp>
2025-10-08 11:50:51 +0700

    test

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World Update!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
